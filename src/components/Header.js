c4585a0eaff05f153ffab3051446de8ecce25185
Devendranath Reddy <113197660+DevendranathReddy7@users.noreply.github.com>
2024-08-17 10:42:40 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
+import {Link} from 'react-router-dom'
+
 const Header = () => (
-  <div>
+  <Link to="/">
     <img
       src="https://assets.ccbp.in/frontend/react-js/tech-era/website-logo-img.png"
       alt="website logo"
       style={{width: '100px'}}
     />
-  </div>
+  </Link>
 )
 
 export default Header
